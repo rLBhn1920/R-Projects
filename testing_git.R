3edc472d741eae718dda19f5bf04780ace50db91
@rLBhn1920 <[rlabuhn@gmail.com]>
2024-09-21 23:20:53 -0400

    added comments on terminal - changed to a text file

---
+++ testing_git.R
@@ -0,0 +1,6 @@
+print("This file was created within RStudio")
+
+print("And now it lives on GitHub")
+
+#https://www.geeksforgeeks.org/link-your-github-account-with-r-studio/
+#Link your GitHub Account with R Studio  -  Last Updated : 29 Mar, 2023
